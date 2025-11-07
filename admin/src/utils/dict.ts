2cc5a7d1046767b999c8b56b5996b343c57cedf5
abulo.hoo <abulo.hoo@gmail.com>
2025-11-07 10:45:25 +0800

    feat(admin): 重构字典管理模块，增强类型支持与功能封装

--- admin/src/utils/dict.ts
+++ admin/src/utils/dict.ts
@@ -1,17 +1,105 @@
-// 系统全局字典
+import { useDictStore } from '@/stores/modules/dict'
+export interface DictDataType {
+  label: string
+  value: string | number | boolean
+  colorType: 'success' | 'info' | 'warning' | 'danger' | ''
+  cssClass: string
+}
+
+export interface NumberDictDataType extends DictDataType {
+  value: number
+}
 
 /**
- * @description：用户性别
+ * 根据字典类型获取字典选项列表
+ * @param dictType - 字典类型标识符
+ * @returns 返回指定类型的字典数据列表，如果未找到则返回空数组
  */
-export const genderType = [
-  { label: '男', value: 1 },
-  { label: '女', value: 2 },
-]
+export const getDictOptions = (dictType: string) => {
+  const dictStore = useDictStore()
+  const dictList = (dictStore.getDict(dictType) as DictDataType[]) || []
+  return dictList
+}
+/**
+ * 获取指定字典类型的数字类型字典选项列表
+ * @param dictType 字典类型标识符
+ * @returns 返回转换后的数字类型字典数据数组
+ */
+export const getIntDictOptions = (dictType: string): NumberDictDataType[] => {
+  // 获得通用的 DictDataType 列表
+  const dictOptions: DictDataType[] = getDictOptions(dictType)
+  // 转换成 number 类型的 NumberDictDataType 类型
+  const dictOption: NumberDictDataType[] = []
+  dictOptions.forEach((dict: DictDataType) => {
+    dictOption.push({
+      ...dict,
+      value: parseInt(dict.value + ''),
+    })
+  })
+  return dictOption
+}
+/**
+ * 获取字符串类型的字典选项数据
+ * @param dictType - 字典类型标识符
+ * @returns 返回处理后的字典数据数组，其中每个选项的value值都转换为字符串类型
+ */
+export const getStrDictOptions = (dictType: string) => {
+  const dictOption: DictDataType[] = []
+  const dictOptions: DictDataType[] = getDictOptions(dictType)
+  dictOptions.forEach((dict: DictDataType) => {
+    dictOption.push({
+      ...dict,
+      value: dict.value + '',
+    })
+  })
+  return dictOption
+}
+/**
+ * 获取布尔类型字典选项
+ * @param dictType 字典类型标识符
+ * @returns 转换后的字典数据数组，其中value字段被转换为布尔类型
+ */
+export const getBoolDictOptions = (dictType: string) => {
+  const dictOption: DictDataType[] = []
+  const dictOptions: DictDataType[] = getDictOptions(dictType)
+  dictOptions.forEach((dict: DictDataType) => {
+    dictOption.push({
+      ...dict,
+      value: dict.value + '' === 'true',
+    })
+  })
+  return dictOption
+}
 
 /**
- * @description：用户状态
+ * 获取指定字典类型的指定值对应的字典对象
+ * @param dictType 字典类型
+ * @param value 字典值
+ * @return DictDataType 字典对象
  */
-export const userStatus = [
-  { label: '启用', value: 1, tagType: 'success' },
-  { label: '禁用', value: 0, tagType: 'danger' },
-]
+export const getDictObj = (dictType: string, value: any): DictDataType | undefined => {
+  const dictOptions: DictDataType[] = getDictOptions(dictType)
+  for (const dict of dictOptions) {
+    if (dict.value === value + '') {
+      return dict
+    }
+  }
+}
+
+/**
+ * 获得字典数据的文本展示
+ *
+ * @param dictType 字典类型
+ * @param value 字典数据的值
+ * @return 字典名称
+ */
+export const getDictLabel = (dictType: string, value: any): string => {
+  const dictOptions: DictDataType[] = getDictOptions(dictType)
+  const dictLabel = ref('')
+  dictOptions.forEach((dict: DictDataType) => {
+    if (dict.value === value + '') {
+      dictLabel.value = dict.label
+    }
+  })
+  return dictLabel.value
+}
--- admin/src/views/sys/dict/index.vue
+++ admin/src/views/sys/dict/index.vue
@@ -1,52 +1,54 @@
 <template>
   <div class="table-box">
-	<ProTable
-	  ref="proTable"
-	  title="字典类型列表"
-	  row-key="id"
-    :columns="columns"
-    :toolbar-right="['search', 'refresh', 'export', 'layout']"
-    :request-api="getTableList"
-    :request-auto="true"
-	  :pagination="true"
-	  :search-col="12">
-	    <template #toolbarLeft>
-        <el-button v-auth="'dict.SysDictTypeCreate'" type="primary" :icon="CirclePlus" @click="handleAdd">新增</el-button>
+    <ProTable
+      ref="proTable"
+      title="字典类型列表"
+      row-key="id"
+      :columns="columns"
+      :toolbar-right="['search', 'refresh', 'export', 'layout']"
+      :request-api="getTableList"
+      :request-auto="true"
+      :pagination="true"
+      :search-col="12"
+    >
+      <template #toolbarLeft>
+        <el-button v-auth="'dict.SysDictTypeCreate'" type="primary" :icon="CirclePlus" @click="handleAdd"
+          >新增</el-button
+        >
       </template>
-	  <!-- 菜单操作 -->
-	  <template #operation="scope">
-      <el-button v-auth="'dict.SysDictType'" type="primary" link :icon="View" @click="handleItem(scope.row)">
-        查看
-      </el-button>
-      <el-dropdown trigger="click">
-        <el-button
-              v-auth="[
-                'dict.SysDictTypeUpdate',
-                'dict.SysDictTypeDelete',
-              ]"
-              type="primary"
-              link
-              :icon="DArrowRight">
-              更多
+      <!-- 菜单操作 -->
+      <template #operation="scope">
+        <el-button v-auth="'dict.SysDictType'" type="primary" link :icon="View" @click="handleItem(scope.row)">
+          查看
         </el-button>
-        <template #dropdown>
-          <el-dropdown-menu>
-            <el-dropdown-item v-auth="'dict.SysDictTypeUpdate'" :icon="EditPen" @click="handleUpdate(scope.row)">
-              编辑
-            </el-dropdown-item>
-            <el-dropdown-item
-              v-if="scope.row.deleted === 0"
-              v-auth="'dict.SysDictTypeDelete'"
-              :icon="Delete"
-              @click="handleDelete(scope.row)">
-              删除
-            </el-dropdown-item>
-          </el-dropdown-menu>
-        </template>
-      </el-dropdown>
-    </template>
-	</ProTable>
-	<el-dialog
+        <el-dropdown trigger="click">
+          <el-button
+            v-auth="['dict.SysDictTypeUpdate', 'dict.SysDictTypeDelete']"
+            type="primary"
+            link
+            :icon="DArrowRight"
+          >
+            更多
+          </el-button>
+          <template #dropdown>
+            <el-dropdown-menu>
+              <el-dropdown-item v-auth="'dict.SysDictTypeUpdate'" :icon="EditPen" @click="handleUpdate(scope.row)">
+                编辑
+              </el-dropdown-item>
+              <el-dropdown-item
+                v-if="scope.row.deleted === 0"
+                v-auth="'dict.SysDictTypeDelete'"
+                :icon="Delete"
+                @click="handleDelete(scope.row)"
+              >
+                删除
+              </el-dropdown-item>
+            </el-dropdown-menu>
+          </template>
+        </el-dropdown>
+      </template>
+    </ProTable>
+    <el-dialog
       v-model="dialogVisible"
       :title="title"
       width="40%"
@@ -56,66 +58,61 @@
       append-to-body
       draggable
       :lock-scroll="false"
-      class="dialog-settings">
-	  <el-form ref="refSysDictTypeForm" :model="sysDictTypeForm" :rules="rulesSysDictTypeForm" label-width="100px">
-		<el-form-item label="编号" prop="id">
+      class="dialog-settings"
+    >
+      <el-form ref="refSysDictTypeForm" :model="sysDictTypeForm" :rules="rulesSysDictTypeForm" label-width="100px">
+        <el-form-item label="编号" prop="id">
           <el-input v-model="sysDictTypeForm.id" :disabled="disabled" />
-    </el-form-item>
-		<el-form-item label="字典名称" prop="name">
+        </el-form-item>
+        <el-form-item label="字典名称" prop="name">
           <el-input v-model="sysDictTypeForm.name" :disabled="disabled" />
-    </el-form-item>
-		<el-form-item label="字典类型" prop="type">
+        </el-form-item>
+        <el-form-item label="字典类型" prop="type">
           <el-input v-model="sysDictTypeForm.type" :disabled="disabled" />
-    </el-form-item>
-		<el-form-item label="状态:0正常/1停用" prop="status">
+        </el-form-item>
+        <el-form-item label="状态:0正常/1停用" prop="status">
           <el-input v-model="sysDictTypeForm.status" :disabled="disabled" />
-    </el-form-item>
-		<el-form-item label="备注" prop="remark">
+        </el-form-item>
+        <el-form-item label="备注" prop="remark">
           <el-input v-model="sysDictTypeForm.remark" :disabled="disabled" />
-    </el-form-item>
-		<el-form-item label="创建人" prop="creator">
+        </el-form-item>
+        <el-form-item label="创建人" prop="creator">
           <el-input v-model="sysDictTypeForm.creator" :disabled="disabled" />
-    </el-form-item>
-		<el-form-item label="创建时间" prop="createTime">
+        </el-form-item>
+        <el-form-item label="创建时间" prop="createTime">
           <el-input v-model="sysDictTypeForm.createTime" :disabled="disabled" />
-    </el-form-item>
-		<el-form-item label="更新人" prop="updater">
+        </el-form-item>
+        <el-form-item label="更新人" prop="updater">
           <el-input v-model="sysDictTypeForm.updater" :disabled="disabled" />
-    </el-form-item>
-		<el-form-item label="更新时间" prop="updateTime">
+        </el-form-item>
+        <el-form-item label="更新时间" prop="updateTime">
           <el-input v-model="sysDictTypeForm.updateTime" :disabled="disabled" />
-    </el-form-item>
-	  </el-form>
-	  <template v-if="!disabled" #footer>
-		<span class="dialog-footer">
-		  <el-button @click="resetForm(refSysDictTypeForm)">取消</el-button>
-		  <el-button type="primary" :loading="loading" @click="submitForm(refSysDictTypeForm)">确定</el-button>
-		</span>
-	  </template>
-	</el-dialog>
+        </el-form-item>
+      </el-form>
+      <template v-if="!disabled" #footer>
+        <span class="dialog-footer">
+          <el-button @click="resetForm(refSysDictTypeForm)">取消</el-button>
+          <el-button type="primary" :loading="loading" @click="submitForm(refSysDictTypeForm)">确定</el-button>
+        </span>
+      </template>
+    </el-dialog>
   </div>
 </template>
 <script setup lang="tsx">
 defineOptions({ name: 'SysDictType' })
 import type { ResSysDictType } from '@/api/interface/sysDictType'
 import type { ProTableInstance, ColumnProps, SearchProps } from '@/components/ProTable/interface'
-import {
-	EditPen,
-	CirclePlus,
-	Delete,
-	View,
-	DArrowRight,
-	} from "@element-plus/icons-vue";
+import { EditPen, CirclePlus, Delete, View, DArrowRight } from '@element-plus/icons-vue'
 import {
   getSysDictTypeListApi,
   deleteSysDictTypeApi,
   getSysDictTypeApi,
   addSysDictTypeApi,
   updateSysDictTypeApi,
-} from "@/api/modules/sysDictType";
+} from '@/api/modules/sysDictType'
 import type { FormInstance, FormRules } from 'element-plus'
-import { useHandleData, useHandleSet } from "@/hooks/useHandleData";
-import { HasPermission } from "@/utils/permission";
+import { useHandleData, useHandleSet } from '@/hooks/useHandleData'
+import { HasPermission } from '@/utils/permission'
 //加载
 const loading = ref(false)
 //禁用
@@ -128,30 +125,26 @@ const proTable = ref<ProTableInstance>()
 const dialogVisible = ref(false)
 //数据接口
 const sysDictTypeForm = ref<ResSysDictType>({
-		id: 0, // 编号
-		name: "", // 字典名称
-		type: "", // 字典类型
-		status: 0, // 状态:0正常/1停用
-		remark: "", // 备注
-		creator: undefined, // 创建人
-		createTime: undefined, // 创建时间
-		updater: undefined, // 更新人
-		updateTime: undefined, // 更新时间
-
-});
+  id: 0, // 编号
+  name: '', // 字典名称
+  type: '', // 字典类型
+  status: 0, // 状态:0正常/1停用
+  remark: '', // 备注
+  creator: undefined, // 创建人
+  createTime: undefined, // 创建时间
+  updater: undefined, // 更新人
+  updateTime: undefined, // 更新时间
+})
 //表单
 const refSysDictTypeForm = ref<FormInstance>()
 //校验
 const rulesSysDictTypeForm = reactive<FormRules>({
-  	id: [{ required: true, message: "编号不能为空", trigger: "blur" }],
-	name: [{ required: true, message: "字典名称不能为空", trigger: "blur" }],
-	type: [{ required: true, message: "字典类型不能为空", trigger: "blur" }],
-	status: [{ required: true, message: "状态:0正常/1停用不能为空", trigger: "blur" }],
-	remark: [{ required: true, message: "备注不能为空", trigger: "blur" }],
-
-});
-
-
+  id: [{ required: true, message: '编号不能为空', trigger: 'blur' }],
+  name: [{ required: true, message: '字典名称不能为空', trigger: 'blur' }],
+  type: [{ required: true, message: '字典类型不能为空', trigger: 'blur' }],
+  status: [{ required: true, message: '状态:0正常/1停用不能为空', trigger: 'blur' }],
+  remark: [{ required: true, message: '备注不能为空', trigger: 'blur' }],
+})
 
 /**
  * 获取表格数据列表
@@ -172,16 +165,15 @@ const getTableList = (params: any) => {
 const resetSysDictType = () => {
   // 设置sysDictTypeForm为默认值
   Object.assign(sysDictTypeForm.value, {
-    		id: 0, // 编号
-		name: "", // 字典名称
-		type: "", // 字典类型
-		status: 0, // 状态:0正常/1停用
-		remark: "", // 备注
-		creator: undefined, // 创建人
-		createTime: undefined, // 创建时间
-		updater: undefined, // 更新人
-		updateTime: undefined, // 更新时间
-
+    id: 0, // 编号
+    name: '', // 字典名称
+    type: '', // 字典类型
+    status: 0, // 状态:0正常/1停用
+    remark: '', // 备注
+    creator: undefined, // 创建人
+    createTime: undefined, // 创建时间
+    updater: undefined, // 更新人
+    updateTime: undefined, // 更新时间
   })
 }
 
@@ -247,7 +239,6 @@ const resetForm = (formEl: FormInstance | undefined) => {
   formEl.resetFields()
 }
 
-
 // 提交数据
 const submitForm = (formEl: FormInstance | undefined) => {
   if (!formEl) return
@@ -267,27 +258,24 @@ const submitForm = (formEl: FormInstance | undefined) => {
 }
 
 const columns: ColumnProps<ResSysDictType>[] = [
-	  	{ prop: "id", label: "编号" },
-	{ prop: "name", label: "字典名称", search: { valueType: "input" } },
-	{ prop: "type", label: "字典类型", search: { valueType: "input" } },
-	{ prop: "status", label: "状态:0正常/1停用", search: { valueType: "input" } },
-	{ prop: "remark", label: "备注" },
-	{ prop: "creator", label: "创建人" },
-	{ prop: "createTime", label: "创建时间" },
-	{ prop: "updater", label: "更新人" },
-	{ prop: "updateTime", label: "更新时间" },
+  { prop: 'id', label: '编号' },
+  { prop: 'name', label: '字典名称', search: { valueType: 'input' } },
+  { prop: 'type', label: '字典类型', search: { valueType: 'input' } },
+  { prop: 'status', label: '状态:0正常/1停用', search: { valueType: 'input' } },
+  { prop: 'remark', label: '备注' },
+  { prop: 'creator', label: '创建人' },
+  { prop: 'createTime', label: '创建时间' },
+  { prop: 'updater', label: '更新人' },
+  { prop: 'updateTime', label: '更新时间' },
 
-	  {
-		prop: "operation",
-		label: "操作",
-		width: 150,
-		fixed: "right",
-		isShow: HasPermission(
-      "dict.SysDictTypeUpdate",
-      "dict.SysDictTypeDelete",
-      "dict.SysDictType",
-    )}
-];
+  {
+    prop: 'operation',
+    label: '操作',
+    width: 150,
+    fixed: 'right',
+    isShow: HasPermission('dict.SysDictTypeUpdate', 'dict.SysDictTypeDelete', 'dict.SysDictType'),
+  },
+]
 </script>
 <style scoped lang="scss">
 @use '@/styles/custom';
