86fc795a1cb10601fca9c460c9f4e621f941cbf2
abulo.hoo <abulo.hoo@gmail.com>
2025-11-08 08:57:16 +0800

    feat(auth): 优化按钮权限指令逻辑

--- admin/src/directives/modules/auth.ts
+++ admin/src/directives/modules/auth.ts
@@ -2,23 +2,20 @@
  * v-auth
  * 按钮权限指令
  */
-import { useAuthStore } from '@/stores/modules/auth'
-import type { Directive, DirectiveBinding } from 'vue'
+import { HasAuthItem } from '@/utils/auth'
 
 const auth: Directive = {
   mounted(el: HTMLElement, binding: DirectiveBinding) {
     const { value } = binding
-    const authStore = useAuthStore()
-    const currentPageRoles = authStore.allAuthButtonList[authStore.routeName] ?? []
     if (value instanceof Array && value.length) {
-      const hasPermission = value.every(item => currentPageRoles.includes(item))
-      if (!hasPermission) {
-        el.remove()
+      let hasPermission = false
+      for (let index = 0; index < value.length; index++) {
+        hasPermission = HasAuthItem(value[index])
+        if (hasPermission) break
       }
+      if (!hasPermission) el.remove()
     } else {
-      if (!currentPageRoles.includes(value)) {
-        el.remove()
-      }
+      if (!HasAuthItem(value)) el.remove()
     }
   },
 }
